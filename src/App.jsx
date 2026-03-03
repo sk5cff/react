9201d16d326daa43a8d35211a88c5d72386950b6
user1 <tatarenkosr24@tula.ithub.ru>
2026-03-03 12:59:08 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Sidebar from './components/Sidebar'
+import WorkSpace from './components/WorkSpace'
+
+function App() {
+
+  return (
+    <div className='global-wrap'>
+      <Sidebar />
+      <WorkSpace />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/WorkSpace.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const WorkSpace = () => {
+    return (
+        <div className='work-space'>
+            
+        </div>
+    )
+}
+
+export default WorkSpace
