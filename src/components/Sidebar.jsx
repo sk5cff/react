41cfa12602ff5f27420a25a5ce453ff70a5c2b21
unknown <tatarenkosr24@tula.ithub.ru>
2026-03-11 11:31:23 +0300

    react

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import profile from "../assets/profile.png"
+import cloud from "../assets/cloud.png"
+import folder from "../assets/folder.png"
+import not_sorted from "../assets/not_sorted.png"
 
 const Sidebar = () => {
     return (
         <div className='sidebar'>
-            
+            <img src={profile} width="35" height="35" alt="" />
+            <img src={cloud} width="35" height="35" alt="" />
+            <div>
+                <img src={not_sorted} width="35" height="35" alt="" />
+                <p>Коллекции</p>
+            </div>
+            <img src={folder} width="35" height="35" alt="" />
         </div>
     )
 }
--- src/components/WorkSpace.jsx
+++ src/components/WorkSpace.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import search from "../assets/search.png"
+import plus from "../assets/plus.png"
 
 const WorkSpace = () => {
     return (
         <div className='work-space'>
-            
+            <div className='header'>
+                <div>
+                    <img src={search} width="35" height="35" alt="" />
+                    <p>Поиск</p>
+                </div>
+                <img src={plus} width="35" height="35" alt="" />
+                
+            </div>
         </div>
     )
 }
