416f1753a66c854f42b6330d243584e27d045106
user1 <tatarenkosr24@tula.ithub.ru>
2026-03-20 11:24:14 +0300

    react

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,18 +3,29 @@ import profile from "../assets/profile.png"
 import cloud from "../assets/cloud.png"
 import folder from "../assets/folder.png"
 import not_sorted from "../assets/not_sorted.png"
+import plus from "../assets/plus.png"
 
 const Sidebar = () => {
     return (
+        
         <div className='sidebar'>
             <img src={profile} width="35" height="35" alt="" />
+            <img src={plus} width="35" height="35" alt="" />
             <img src={cloud} width="35" height="35" alt="" />
-            <div>
-                <img src={not_sorted} width="35" height="35" alt="" />
-                <p>Коллекции</p>
-            </div>
+                <p>Все закладки</p>
+            <img src={not_sorted} width="35" height="35" alt="" />
+                <p>Несортированные</p>
+            <p>Коллекции</p>
             <img src={folder} width="35" height="35" alt="" />
+                <p>fff</p>
+
+                <div className='add'> 
+                    <p>Бесконечные возможности для организации ваших закладок</p>
+                    <p>Перейти на PRO</p>
+                </div>
         </div>
+        
+
     )
 }
 
--- src/components/WorkSpace.jsx
+++ src/components/WorkSpace.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import search from "../assets/search.png"
 import plus from "../assets/plus.png"
+import time from "../assets/time.png"
+import list from "../assets/list.png"
+import install from "../assets/install.png"
+import share from "../assets/share.png"
+import cloud from "../assets/cloud.png"
 
 const WorkSpace = () => {
     return (
@@ -8,11 +13,23 @@ const WorkSpace = () => {
             <div className='header'>
                 <div>
                     <img src={search} width="35" height="35" alt="" />
-                    <p>Поиск</p>
+                        <p>Поиск</p>
+                    <img src={plus} width="35" height="35" alt="" />
+                        <p>Добавить</p>
+                        <div>
+                            <img src={cloud} width="35" height="35" alt="" />
+                                <p>Добавить</p>
+                            <img src={time} width="25" height="25" alt="" />
+                                <p>По дате</p>
+                            <img src={list} width="25" height="25" alt="" />
+                                <p>Списком</p>
+                        </div>
                 </div>
-                <img src={plus} width="35" height="35" alt="" />
-                
             </div>
+            <img src={install} width="20" height="20" alt="" />
+            <p>Установить расширение для браузера</p>
+            <img src={share} width="20" height="20" alt="" />
+            <p>Импортировать закладку</p>
         </div>
     )
 }
